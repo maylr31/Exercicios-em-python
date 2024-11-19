2b8f956f3d47e9219ffa9b0d21e5eb5ce10e64f7
maylr31 <maylopes2122@gmail.com>
2024-11-19 20:44:56 -0300

    ..

--- exercicio 2.2.py
+++ exercicio 2.2.py
@@ -1,4 +1,4 @@
-# Entrada dos dados
+# Entrada dos dados, eu não uso a função def pois ela é para chamar em novos codigos e deixar esse salvo,não vejo necessidade
 numero = int(input("Digite o número do funcionário: "))
 horas_trabalhadas = int(input("Digite o número de horas trabalhadas: "))
 valor_por_hora = float(input("Digite o valor por hora: "))
